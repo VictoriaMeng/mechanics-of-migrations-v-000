5cb0fa7d565c9fb246b3c70d6abca9eba3643c55
Victoria Meng <nights1ststar@protonmail.com>
2018-05-12 21:02:11 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -18,7 +18,7 @@ connection = ActiveRecord::Base.establish_connection(
 
 sql = <<-SQL
   CREATE TABLE IF NOT EXISTS artists (
-    id INTEGER PRIMARY key
+    id INTEGER PRIMARY key,
     name TEXT,
     genre TEXT,
     age INTEGER,
